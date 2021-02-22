925dc8ca4cb9f9461ac844cd6809f0ca06b4099d
Charalampos Hadjiantoniou <charhadj@gmail.com>
2021-02-22 11:42:56 +0200

    Implementation of get int

--- PamposTools.InShell/Helpers/InputHelper.cs
+++ PamposTools.InShell/Helpers/InputHelper.cs
@@ -8,7 +8,7 @@ namespace PamposTools.InShell
     {
         public static bool GetYesOrNo(string promptMessage) {
             do {
-                PrintHelper.Print($"{promptMessage}");
+                PrintHelper.Print($"{promptMessage}  ");
 
                 string response = Console.ReadLine()?.ToLower()?.Trim();
 
@@ -20,11 +20,47 @@ namespace PamposTools.InShell
                     case "yes":
                         return true;
                     default:
-                        Console.WriteLine($"Invalid response '{response}'. Please answer 'y' or 'n' or CTRL+C to exit.");
+                        PrintHelper.PrintLine($"Invalid response '{response}'. Please answer 'y' or 'n' or CTRL+C to exit.", LogLevel.Warning);
                         break;
                 }
             }
             while (true);
+        }
+
+        public static string GetString(string promptMessage) {
+            PrintHelper.Print($"{promptMessage}  ");
+            return Console.ReadLine();
+        }
+
+        public static int GetInt(string promptMessage) {
+            do {
+                PrintHelper.Print($"{promptMessage}  ");
+                string? resp = Console.ReadLine()?.ToLower()?.Trim();
+
+                if (string.IsNullOrEmpty(resp)) {
+                    PrintHelper.PrintLine("Please enter a valid number or press CTRL+C to exit.", LogLevel.Warning);
+                    continue;
+                }
+
+                if (int.TryParse(resp, out var result)) {
+                    return result;
+                }
+
+                PrintHelper.PrintLine($"Invalid number '{resp}'. Please enter a valid number or press CTRL+C to exit.", LogLevel.Warning);
+            }
+            while (true);
+        }
+
+        public static int GetInt(string promptMessage, int min, int max) {
+            do {
+                int integer = GetInt(promptMessage);
+
+                if (integer < min || integer > max) {
+                    PrintHelper.PrintLine($"Please enter a number between {min} and {max} inclusive");
+                    continue;
+                }
+                return integer;
+            } while (true);
         }
     }
 }
